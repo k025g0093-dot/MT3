d35f76ec39b6bf437553f56e8e797e979508dc25
ヤマト_ユウヤ <k025g0093@g.neec.ac.jp>
2026-04-24 10:56:16 +0900

    00_03確認課題終了

--- Vector.h
+++ Vector.h
@@ -5,6 +5,10 @@ struct Matrix4x4
 	float m[4][4];
 };
 
+struct Vector3 {
+	float x, y, z;
+};
+
 Matrix4x4 Add(const Matrix4x4& m1, const Matrix4x4& m2);
 Matrix4x4 Subtract(const Matrix4x4& m1, const Matrix4x4& m2);
 Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4& m2);
@@ -12,5 +16,11 @@ Matrix4x4 Transpose(const Matrix4x4& m);
 Matrix4x4 Inverse(const Matrix4x4& m);
 Matrix4x4 MakeIdentity4x4();
 
+//平行移動
+Matrix4x4 MakeTranslateMatrix(const Vector3& Vector);
+//拡縮
+Matrix4x4 MakeScaleMatrix(const Vector3& Vector);
+//座標変換
+Vector3 Transform(const Vector3& Vector, const Matrix4x4& matrix);
 
 
