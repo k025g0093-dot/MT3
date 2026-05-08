f7a3364c9a5c3a748d2c6d71cbc1ac45428fb9cb
ヤマト_ユウヤ <k025g0093@g.neec.ac.jp>
2026-05-08 10:29:49 +0900

    確認課題終了
    やったこと
    
    アフィン変換行列の作成をやったよ

--- Vector.h
+++ Vector.h
@@ -26,3 +26,5 @@ Vector3 Transform(const Vector3& Vector, const Matrix4x4& matrix);
 Matrix4x4 MakeRotateXMatrix(float radian);
 Matrix4x4 MakeRotateYMatrix(float radian);
 Matrix4x4 MakeRotateZMatrix(float radian);
+
+Matrix4x4 MakeAffineMatrix(const Vector3& scale, const Vector3& rotate, const Vector3& translate);
