92ece509509ea011d1f9b05c962f49f11f97f34c
ヤマト_ユウヤ <k025g0093@g.neec.ac.jp>
2026-05-08 11:30:17 +0900

    確認課題終了
    やったこと
    ビューポートバージョン２にアップデート

--- Vector.h
+++ Vector.h
@@ -28,3 +28,9 @@ Matrix4x4 MakeRotateYMatrix(float radian);
 Matrix4x4 MakeRotateZMatrix(float radian);
 
 Matrix4x4 MakeAffineMatrix(const Vector3& scale, const Vector3& rotate, const Vector3& translate);
+
+Matrix4x4 MakePerspectiveFovMatrix(float fovY, float aspect, float nearClip, float farClip);
+
+Matrix4x4 MakeOrthographicMatrix(float left, float top, float right, float bottom, float nearClip, float farClip);
+
+Matrix4x4 MakeViewportMatrix(float left, float top, float width, float height, float minDepth, float maxDepth);
