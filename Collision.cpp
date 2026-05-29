e64b5ca6f229f87718c82cefb89160485bdda803
ヤマト_ユウヤ <k025g0093@g.neec.ac.jp>
2026-05-29 11:12:26 +0900

    球と四角の当たり判定を実装確認課題終了

--- Collision.cpp
+++ Collision.cpp
@@ -2,7 +2,7 @@
 
 bool IsCollision(const Sphere& s1, const Sphere& s2) {
 
-	float distnce = Length(SubtractVector3(s2.center , s1.center));
+	float distnce = Length(SubtractVector3(s2.center, s1.center));
 
 	if (distnce <= s1.radius + s2.radius) {
 		return true;
@@ -12,87 +12,103 @@ bool IsCollision(const Sphere& s1, const Sphere& s2) {
 
 bool IsCollisionPlane(const Sphere& s1, const Plane& p1) {
 
-    float distance = Dot(p1.normal, s1.center) - p1.distance;
-    float absDistance = std::abs(distance);
-    if (absDistance <= s1.radius) {
-        return true;
-    }
-    return false;
+	float distance = Dot(p1.normal, s1.center) - p1.distance;
+	float absDistance = std::abs(distance);
+	if (absDistance <= s1.radius) {
+		return true;
+	}
+	return false;
 }
 
 bool isLineCollisionPlame(
-    const Segment& segment,
-    const Plane& plane)
+	const Segment& segment,
+	const Plane& plane)
 {
 
-    float dot = Dot(plane.normal, segment.diff);
-    if (dot == 0.0f) {
-        return false;
-    }
+	float dot = Dot(plane.normal, segment.diff);
+	if (dot == 0.0f) {
+		return false;
+	}
 
-    float t = (plane.distance - Dot(segment.origin, plane.normal)) / dot;
+	float t = (plane.distance - Dot(segment.origin, plane.normal)) / dot;
 
 
-    if (t >= 0.0f && t <= 1.0f) {
-        return true;
-    }
-    return false;
+	if (t >= 0.0f && t <= 1.0f) {
+		return true;
+	}
+	return false;
 
 }
 
 
 
 bool isTraiangleCollisionLine(
-    const Triangle& tri,
-    const Segment& segment)
+	const Triangle& tri,
+	const Segment& segment)
 {
-    Vector3 edge01 = SubtractVector3(tri.vertices[1], tri.vertices[0]);
-    Vector3 edge12 = SubtractVector3(tri.vertices[2], tri.vertices[1]);
-    Vector3 edge20 = SubtractVector3(tri.vertices[0], tri.vertices[2]);
+	Vector3 edge01 = SubtractVector3(tri.vertices[1], tri.vertices[0]);
+	Vector3 edge12 = SubtractVector3(tri.vertices[2], tri.vertices[1]);
+	Vector3 edge20 = SubtractVector3(tri.vertices[0], tri.vertices[2]);
 
-    Vector3 normal = Normalize(Cross(edge01, edge12));
-    float distance = Dot(normal, tri.vertices[0]);
+	Vector3 normal = Normalize(Cross(edge01, edge12));
+	float distance = Dot(normal, tri.vertices[0]);
 
-    Plane plane{ normal, distance };
+	Plane plane{ normal, distance };
 
-    if (!isLineCollisionPlame(segment, plane)) {
-        return false;
-    }
+	if (!isLineCollisionPlame(segment, plane)) {
+		return false;
+	}
 
-    float t = (distance - Dot(segment.origin, normal)) / Dot(normal, segment.diff);
-    Vector3 point = {
-        segment.origin.x + segment.diff.x * t,
-        segment.origin.y + segment.diff.y * t,
-        segment.origin.z + segment.diff.z * t,
-    };
+	float t = (distance - Dot(segment.origin, normal)) / Dot(normal, segment.diff);
+	Vector3 point = {
+		segment.origin.x + segment.diff.x * t,
+		segment.origin.y + segment.diff.y * t,
+		segment.origin.z + segment.diff.z * t,
+	};
 
-    Vector3 cross01 = Cross(edge01, SubtractVector3(point, tri.vertices[0]));
-    Vector3 cross12 = Cross(edge12, SubtractVector3(point, tri.vertices[1]));
-    Vector3 cross20 = Cross(edge20, SubtractVector3(point, tri.vertices[2]));
+	Vector3 cross01 = Cross(edge01, SubtractVector3(point, tri.vertices[0]));
+	Vector3 cross12 = Cross(edge12, SubtractVector3(point, tri.vertices[1]));
+	Vector3 cross20 = Cross(edge20, SubtractVector3(point, tri.vertices[2]));
 
-    if (Dot(normal, cross01) >= 0.0f &&
-        Dot(normal, cross12) >= 0.0f &&
-        Dot(normal, cross20) >= 0.0f) {
-        return true;
-    }
+	if (Dot(normal, cross01) >= 0.0f &&
+		Dot(normal, cross12) >= 0.0f &&
+		Dot(normal, cross20) >= 0.0f) {
+		return true;
+	}
 
-    return false;
+	return false;
 }
 
 bool isAABBToAABBCollision(
-    const AABB& aabb1,
-    const AABB& aabb2
+	const AABB& aabb1,
+	const AABB& aabb2
 )
 {
 
-    if (
-        (aabb1.min.x <= aabb2.max.x && aabb1.max.x >= aabb2.min.x) &&
-        (aabb1.min.y <= aabb2.max.y && aabb1.max.y >= aabb2.min.y) &&
-        (aabb1.min.z <= aabb2.max.z && aabb1.max.z >= aabb2.min.z))
-    {
-        return true;
-    }
-    return false;
+	if (
+		(aabb1.min.x <= aabb2.max.x && aabb1.max.x >= aabb2.min.x) &&
+		(aabb1.min.y <= aabb2.max.y && aabb1.max.y >= aabb2.min.y) &&
+		(aabb1.min.z <= aabb2.max.z && aabb1.max.z >= aabb2.min.z))
+	{
+		return true;
+	}
+	return false;
 }
 
+bool isAABBToSphereCollision(
+	const AABB& aabb,
+	const Sphere& sphere
+) {
+
+	Vector3 closestPoint = {
+		std::clamp(sphere.center.x,aabb.min.x,aabb.max.x),
+		std::clamp(sphere.center.y,aabb.min.y,aabb.max.y),
+		std::clamp(sphere.center.z,aabb.min.z,aabb.max.z),
+	};
+	float distance = Length(SubtractVector3(sphere.center, closestPoint));
+	if (distance <= sphere.radius) {
+		return true;
+	}
+	return false;
+}
 
